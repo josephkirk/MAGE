9ca09dd234bc935133816e40045b78d85aab356d
Nguyen Phi Hung <josephkirk.art@gmail.com>
2024-12-22 18:07:36 +0700

    Add Bluepritable system

---
+++ Source/MAGECore/Private/Systems/UMAGEBlueprintSystemBase.cpp
@@ -0,0 +1,161 @@
+﻿#include "Systems/MAGEBlueprintSystemBase.h"
+#include "Components/MAGEComponent.h"
+#include "DrawDebugHelpers.h"
+
+UMAGEBlueprintSystemBase::UMAGEBlueprintSystemBase()
+{
+    bAutoActivate = true;
+    BlueprintUpdatePriority = 0;
+}
+
+void UMAGEBlueprintSystemBase::Initialize()
+{
+    Super::Initialize();
+    
+    // Update required tags from blueprint configuration
+    RequiredComponentTags.AppendTags(RequiredComponentTagsForBlueprint);
+    UpdatePriority = BlueprintUpdatePriority;
+    
+    // Call blueprint event
+    ReceiveInitialize();
+}
+
+void UMAGEBlueprintSystemBase::Shutdown()
+{
+    // Call blueprint event
+    ReceiveShutdown();
+    
+    Super::Shutdown();
+}
+
+void UMAGEBlueprintSystemBase::Update(float DeltaTime)
+{
+    Super::Update(DeltaTime);
+    
+    // Broadcast tick event
+    OnSystemTick.Broadcast(DeltaTime);
+    
+    // Call blueprint event
+    ReceiveUpdate(DeltaTime);
+}
+
+void UMAGEBlueprintSystemBase::OnComponentRegistered(UMAGEComponent* Component)
+{
+    Super::OnComponentRegistered(Component);
+    
+    if (Component)
+    {
+        // Broadcast component registered event
+        OnComponentRegisteredEvent.Broadcast(Component);
+        
+        // Call blueprint event
+        ReceiveComponentRegistered(Component);
+    }
+}
+
+void UMAGEBlueprintSystemBase::OnComponentUnregistered(UMAGEComponent* Component)
+{
+    if (Component)
+    {
+        // Broadcast component unregistered event
+        OnComponentUnregisteredEvent.Broadcast(Component);
+        
+        // Call blueprint event
+        ReceiveComponentUnregistered(Component);
+    }
+    
+    Super::OnComponentUnregistered(Component);
+}
+
+TArray<UMAGEComponent*> UMAGEBlueprintSystemBase::GetRegisteredComponents() const
+{
+    TArray<UMAGEComponent*> Result;
+    for (const auto& WeakComponent : RegisteredComponents)
+    {
+        if (UMAGEComponent* Component = WeakComponent.Get())
+        {
+            Result.Add(Component);
+        }
+    }
+    return Result;
+}
+
+TArray<UMAGEComponent*> UMAGEBlueprintSystemBase::GetRegisteredComponentsByClass(TSubclassOf<UMAGEComponent> ComponentClass) const
+{
+    TArray<UMAGEComponent*> Result;
+    if (!ComponentClass) return Result;
+    
+    for (const auto& WeakComponent : RegisteredComponents)
+    {
+        if (UMAGEComponent* Component = WeakComponent.Get())
+        {
+            if (Component->IsA(ComponentClass))
+            {
+                Result.Add(Component);
+            }
+        }
+    }
+    return Result;
+}
+
+TArray<UMAGEComponent*> UMAGEBlueprintSystemBase::GetRegisteredComponentsByTags(const FGameplayTagContainer& Tags, bool bRequireAll) const
+{
+    TArray<UMAGEComponent*> Result;
+    if (Tags.IsEmpty()) return Result;
+    
+    for (const auto& WeakComponent : RegisteredComponents)
+    {
+        if (UMAGEComponent* Component = WeakComponent.Get())
+        {
+            const FGameplayTagContainer& ComponentTags = Component->GetMageComponentTags();
+            if (bRequireAll ? ComponentTags.HasAll(Tags) : ComponentTags.HasAny(Tags))
+            {
+                Result.Add(Component);
+            }
+        }
+    }
+    return Result;
+}
+
+void UMAGEBlueprintSystemBase::DrawDebugString(const FString& Text, const FColor& Color, float Duration)
+{
+    if (UWorld* World = GetWorld())
+    {
+        GEngine->AddOnScreenDebugMessage(-1, Duration, Color, Text);
+    }
+}
+
+void UMAGEBlueprintSystemBase::DrawDebugSphere(const FVector& Location, float Radius, const FColor& Color, float Duration)
+{
+    if (UWorld* World = GetWorld())
+    {
+        ::DrawDebugSphere(World, Location, Radius, 12, Color, false, Duration);
+    }
+}
+
+// Default implementations for blueprint events
+void UMAGEBlueprintSystemBase::ReceiveInitialize_Implementation() {}
+void UMAGEBlueprintSystemBase::ReceiveShutdown_Implementation() {}
+void UMAGEBlueprintSystemBase::ReceiveUpdate_Implementation(float DeltaTime) {}
+void UMAGEBlueprintSystemBase::ReceiveComponentRegistered_Implementation(UMAGEComponent* Component) {}
+void UMAGEBlueprintSystemBase::ReceiveComponentUnregistered_Implementation(UMAGEComponent* Component) {}
+
+#if WITH_EDITOR
+void UMAGEBlueprintSystemBase::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+    Super::PostEditChangeProperty(PropertyChangedEvent);
+    
+    // Update system configuration when properties change in editor
+    if (PropertyChangedEvent.Property)
+    {
+        if (PropertyChangedEvent.Property->GetFName() == GET_MEMBER_NAME_CHECKED(UMAGEBlueprintSystemBase, RequiredComponentTagsForBlueprint))
+        {
+            RequiredComponentTags.AppendTags(RequiredComponentTagsForBlueprint);
+        }
+        else if (PropertyChangedEvent.Property->GetFName() == GET_MEMBER_NAME_CHECKED(UMAGEBlueprintSystemBase, BlueprintUpdatePriority))
+        {
+            UpdatePriority = BlueprintUpdatePriority;
+        }
+    }
+}
+#endif
---
+++ Source/MAGECore/Public/Systems/MAGEBlueprintSystemBase.h
@@ -0,0 +1,90 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Systems/MAGESystemBase.h"
+#include "MAGEBlueprintSystemBase.generated.h"
+
+// Delegate for blueprint binding
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMAGEComponentRegisteredDelegate, UMAGEComponent*, Component);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMAGEComponentUnregisteredDelegate, UMAGEComponent*, Component);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMAGESystemTickDelegate, float, DeltaTime);
+
+UCLASS(Blueprintable, Abstract)
+class MAGECORE_API UMAGEBlueprintSystemBase : public UMAGESystemBase
+{
+    GENERATED_BODY()
+
+public:
+    UMAGEBlueprintSystemBase();
+
+    // Core system overrides with blueprint exposure
+    virtual void Initialize() override;
+    virtual void Shutdown() override;
+    virtual void Update(float DeltaTime) override;
+    
+    // Blueprint event delegates
+    UPROPERTY(BlueprintAssignable, Category = "MAGE|Events")
+    FMAGEComponentRegisteredDelegate OnComponentRegisteredEvent;
+    
+    UPROPERTY(BlueprintAssignable, Category = "MAGE|Events")
+    FMAGEComponentUnregisteredDelegate OnComponentUnregisteredEvent;
+    
+    UPROPERTY(BlueprintAssignable, Category = "MAGE|Events")
+    FMAGESystemTickDelegate OnSystemTick;
+
+protected:
+    // Blueprint overridable functions
+    UFUNCTION(BlueprintNativeEvent, Category = "MAGE|System")
+    void ReceiveInitialize();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = "MAGE|System")
+    void ReceiveShutdown();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = "MAGE|System")
+    void ReceiveUpdate(float DeltaTime);
+    
+    UFUNCTION(BlueprintNativeEvent, Category = "MAGE|System")
+    void ReceiveComponentRegistered(UMAGEComponent* Component);
+    
+    UFUNCTION(BlueprintNativeEvent, Category = "MAGE|System")
+    void ReceiveComponentUnregistered(UMAGEComponent* Component);
+
+    // Blueprint helper functions
+    UFUNCTION(BlueprintCallable, Category = "MAGE|Components")
+    TArray<UMAGEComponent*> GetRegisteredComponents() const;
+    
+    UFUNCTION(BlueprintCallable, Category = "MAGE|Components", meta=(DeterminesOutputType="ComponentClass"))
+    TArray<UMAGEComponent*> GetRegisteredComponentsByClass(TSubclassOf<UMAGEComponent> ComponentClass) const;
+    
+    UFUNCTION(BlueprintCallable, Category = "MAGE|Components")
+    TArray<UMAGEComponent*> GetRegisteredComponentsByTags(const FGameplayTagContainer& Tags, bool bRequireAll = true) const;
+    
+    // Configuration properties exposed to blueprints
+    UPROPERTY(EditDefaultsOnly, Category = "MAGE|Configuration")
+    bool bAutoActivate;
+    
+    UPROPERTY(EditDefaultsOnly, Category = "MAGE|Configuration")
+    FGameplayTagContainer RequiredComponentTagsForBlueprint;
+    
+    UPROPERTY(EditDefaultsOnly, Category = "MAGE|Configuration")
+    int32 BlueprintUpdatePriority;
+
+    // Debug helpers
+    UFUNCTION(BlueprintCallable, Category = "MAGE|Debug")
+    void DrawDebugString(const FString& Text, const FColor& Color = FColor::White, float Duration = 0.0f);
+    
+    UFUNCTION(BlueprintCallable, Category = "MAGE|Debug")
+    void DrawDebugSphere(const FVector& Location, float Radius = 100.0f, const FColor& Color = FColor::Red, float Duration = 0.0f);
+
+#if WITH_EDITOR
+    // Editor-only functionality
+    virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+private:
+    // Internal overrides from base system
+    virtual void OnComponentRegistered(UMAGEComponent* Component) override;
+    virtual void OnComponentUnregistered(UMAGEComponent* Component) override;
+};
